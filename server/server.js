c51a1b581f39fccf6006ed117c71b770945bd77e
MacBook Pro <apple@Macs-MacBook.local>
2017-07-06 12:50:35 +0545

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const bodyParser = require ('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require ('./models/todo');
 var {user} = require ('./models/user');
+var {authenticate} = require ('./middleware/authenticate');
 
 var app=express();
 const port = process.env.PORT; //HeroKU
@@ -107,9 +108,7 @@ app.patch('/todos/:id', (req, res) =>{
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var users = new user(body);
-
-
-
+   
     users.save().then(() =>{
        return users.generateAuthToken();
        // res.send(user);
@@ -118,6 +117,11 @@ app.post('/users', (req, res) => {
     }).catch((e)=>{
         res.status(400).send(e);
     });
+});
+
+
+app.get('/users/me',authenticate, (req, res) =>{
+    res.send(req.user);
 });
 
 app.listen(port, () => {

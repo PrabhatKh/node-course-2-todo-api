8b88afc8fcd91b95eb635458bb5093428d966342
MacBook Pro <apple@Macs-MacBook.local>
2017-07-07 09:09:27 +0545

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,29 +1,45 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
+var password = '123abc!';
 
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+bcrypt.genSalt(10, (err, salt) =>{
+   bcrypt.hash(password, salt, (err, hash) =>{
+       console.log(hash);
+   });
+});
 
-var data = {
-    id: 4
-};
-var token ={
-    data,
-    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-};
+// var hashedPassword = '$2a$10$wTRPIJQlo.bxVYQoGizh1eu78xpGjgUw3CO2GkUQX8FpYb34i7MGS';
 
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString();
+// bcrypt.compare(password, hashedPassword, (err, res) =>{
+//     console.log(res);
+// })
+
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//     id: 4
+// };
+// var token ={
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
 
 
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
 
-if(resultHash === token.hash) {
-    console.log('Data was not changed');
-} else {
-    console.log('Data was changed');
-    // console.log(resultHash);
-    // console.log(token.hash);
-}
+// if(resultHash === token.hash) {
+//     console.log('Data was not changed');
+// } else {
+//     console.log('Data was changed');
+//     // console.log(resultHash);
+//     // console.log(token.hash);
+// }

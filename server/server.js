5370adb6d67d4125c095cda464156ab6f82d4eab
MacBook Pro <apple@Macs-MacBook.local>
2017-07-10 10:33:52 +0545

    Add DELETE /usrrs/me/token route

--- server/server.js
+++ server/server.js
@@ -138,6 +138,16 @@ app.post('/users/login', (req, res) =>{
     });
 });
 
+app.delete('users/me/token', authenticate, (req, res) =>{
+     console.log('removing test', token);
+     req.user.removeToken(req.token).then(() =>{
+         
+         res.status(200).send();
+     }, ()=>{
+         res.status(400).send();
+     });
+});
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`)
 });
